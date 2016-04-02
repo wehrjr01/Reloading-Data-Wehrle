45842ab2fe741a32cc04c4439f6907f6b096fc35
jay wehrle <wehrjr01@students.ipfw.edu>
2016-04-02 16:56:23 -0400

    Trying to repair database issues Had the dvg filling based on cdo box, but now can't get it to drop the old names of the database.

---
+++ ReloadingData_Wehrle/AddComponents.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class AddComponents
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.cboComponentType = New System.Windows.Forms.ComboBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.btnHome = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.radRifle = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.errProvider = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.errProvider, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Location = New System.Drawing.Point(594, 208)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(39, 13)
+        Me.lbl4.TabIndex = 81
+        Me.lbl4.Text = "Label4"
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(594, 172)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(39, 13)
+        Me.lbl3.TabIndex = 80
+        Me.lbl3.Text = "Label3"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(594, 137)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(39, 13)
+        Me.lbl2.TabIndex = 79
+        Me.lbl2.Text = "Label2"
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(594, 101)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(39, 13)
+        Me.lbl1.TabIndex = 78
+        Me.lbl1.Text = "Label1"
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(679, 201)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(121, 20)
+        Me.txt4.TabIndex = 5
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(679, 130)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(121, 20)
+        Me.txt2.TabIndex = 3
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(679, 165)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(121, 20)
+        Me.txt3.TabIndex = 4
+        '
+        'cboComponentType
+        '
+        Me.cboComponentType.FormattingEnabled = True
+        Me.cboComponentType.Location = New System.Drawing.Point(120, 32)
+        Me.cboComponentType.Name = "cboComponentType"
+        Me.cboComponentType.Size = New System.Drawing.Size(121, 21)
+        Me.cboComponentType.TabIndex = 1
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(679, 95)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(121, 20)
+        Me.txt1.TabIndex = 2
+        '
+        'btnHome
+        '
+        Me.btnHome.Location = New System.Drawing.Point(679, 337)
+        Me.btnHome.Name = "btnHome"
+        Me.btnHome.Size = New System.Drawing.Size(115, 23)
+        Me.btnHome.TabIndex = 8
+        Me.btnHome.Text = "Return To Home"
+        Me.btnHome.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(211, 163)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 70
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(211, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 69
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(679, 287)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Add Selection"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'radRifle
+        '
+        Me.radRifle.AutoSize = True
+        Me.radRifle.Location = New System.Drawing.Point(679, 238)
+        Me.radRifle.Name = "radRifle"
+        Me.radRifle.Size = New System.Drawing.Size(46, 17)
+        Me.radRifle.TabIndex = 6
+        Me.radRifle.TabStop = True
+        Me.radRifle.Text = "Rifle"
+        Me.radRifle.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 83
+        Me.Label1.Text = "Select item type"
+        '
+        'errProvider
+        '
+        Me.errProvider.ContainerControl = Me
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 71)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(566, 289)
+        Me.DataGridView1.TabIndex = 84
+        '
+        'AddComponents
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(833, 466)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.radRifle)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.txt4)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.cboComponentType)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.btnHome)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button4)
+        Me.Name = "AddComponents"
+        Me.Text = "AddComponents"
+        CType(Me.errProvider, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents cboComponentType As ComboBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents btnHome As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents radRifle As RadioButton
+    Friend WithEvents CartridgeDataSet As CartridgeDataSet
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PowderTableAdapter As CartridgeDataSetTableAdapters.PowderTableAdapter
+    Friend WithEvents CartridgeTableAdapter As CartridgeDataSetTableAdapters.CartridgeTableAdapter
+    Friend WithEvents BulletTableAdapter As CartridgeDataSetTableAdapters.BulletTableAdapter
+    Friend WithEvents errProvider As ErrorProvider
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
